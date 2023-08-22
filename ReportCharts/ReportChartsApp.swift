c6904d32f36af85d842fea7b5bf4218a1c5d63c6
Youssef Ghattas <youssef.ghattas@integrant.com>
2023-08-22 13:34:49 +0300

    Initial Commit

---
+++ ReportCharts/ReportChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReportChartsApp.swift
+//  ReportCharts
+//
+//  Created by Youssef Ghattas on 22/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct ReportChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
